9f8e83b205060c9adf49cb0be0809c8222ed842c
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-04-02 16:11:11 +0900

    feat: Refactor Alphabet class to improve index mapping and overflow handling

--- include/Alphabet.hpp
+++ include/Alphabet.hpp
@@ -13,20 +13,26 @@ class Alphabet {
             throw std::invalid_argument("Q must be at most 36");
         used = BASE.substr(0, Q);
 
-        map.fill(-1);
+        map.fill(SIZE_MAX);
         for (size_t i = 0; i < used.size(); ++i)
-            map[(unsigned char)used[i]] = (int)i;
+            map[(unsigned char)used[i]] = i;
     }
 
     size_t size() const {
         return used.size();
     }
 
-    int toIdx(char c) const {
-        int idx = map[(unsigned char)c];
-        if (idx == -1)
-            throw std::invalid_argument("invalid character");
-        return idx;
+    size_t toValue(const std::string& s) const {
+        size_t value = 0;
+
+        for (char c : s) {
+            size_t digit = toIdx(c);
+            if (value > (SIZE_MAX - digit) / size())
+                throw std::overflow_error("value overflow");
+            value = value * size() + digit;
+        }
+
+        return value;
     }
 
     std::string toSymbol(size_t value, size_t length = 1) const {
@@ -48,11 +54,28 @@ class Alphabet {
         return result;
     }
 
+    std::string add(const std::string& a, const std::string& b) const {
+        std::string result(a.size(), toChar(0));
+        for (size_t i = 0; i < a.size(); ++i) {
+            size_t digitA = toIdx(a[i]);
+            size_t digitB = toIdx(b[i]);
+            result[i] = toChar((digitA + digitB) % size());
+        }
+        return result;
+    }
+
   private:
     static constexpr std::string_view BASE = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     std::string used;
-    std::array<int, 256> map{};
+    std::array<size_t, 256> map{};
+
+    size_t toIdx(char c) const {
+        size_t idx = map[(unsigned char)c];
+        if (idx == SIZE_MAX)
+            throw std::invalid_argument("invalid character");
+        return idx;
+    }
 
     char toChar(size_t i) const {
         if (i >= used.size())

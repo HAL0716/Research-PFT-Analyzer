65d588c9d015d5d83acc91ca5e62341a4af56e5c
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-04-15 16:09:03 +0900

    feat: enhance analyze method to include additional evaluations for all product sets

--- include/Analysis/L4_T1_P1.hpp
+++ include/Analysis/L4_T1_P1.hpp
@@ -16,10 +16,15 @@ namespace Analysis {
             return {
                 evaluate(p[0], p[1], "S0", "S1"),
                 evaluate(p[0], p[2], "S0", "S2"),
-                p[0].size() == util::calcPower(cfg.Q, cfg.T) ? "全" : "非全",
+                evaluate(p[0], p[3], "S0", "S3"),
                 evaluate(p[1], p[2], "S1", "S2"),
                 evaluate(p[1], p[3], "S1", "S3"),
-                evaluate(p[2], p[3], "S2", "S3")};
+                evaluate(p[2], p[3], "S2", "S3"),
+                p[0].size() == util::calcPower(cfg.Q, cfg.T) ? "全" : "非全",
+                p[1].size() == util::calcPower(cfg.Q, cfg.T) ? "全" : "非全",
+                p[2].size() == util::calcPower(cfg.Q, cfg.T) ? "全" : "非全",
+                p[3].size() == util::calcPower(cfg.Q, cfg.T) ? "全" : "非全",
+            };
         }
 
       private:
